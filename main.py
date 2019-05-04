c968de1a432a4f02fbf0be5a566f4becc43bad14
Nick Paisley <2ep303@gmail.com>
2019-05-04 16:24:47 -0500

    finished stage 1

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(120))
+    post = db.Column(db.String(120))
 
-    def __init__(self, name):
-        self.name = name
-
+    def __init__(self, title, post):
+        self.title = title
+        self.post =  post
 
 posts = []
 
@@ -24,26 +24,43 @@ posts = []
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
 
-    if request.method == 'POST':
-        post = request.form['post']
-        posts.append(post)
+    #Query database for all blog posts
+    allblogposts = Blog.query.all()
+    post_id = request.args.get('id')
+    
+    if (post_id):
+        blogpost = Blog.query.get(post_id)
+        return render_template('blogpost.html', blogpost=blogpost)
 
-    return render_template('blog.html', title="Blogs!", posts=posts)
+    return render_template('blog.html', title="Blogs!", posts=allblogposts)
+
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
+    
     posterror = ""
+    allblogposts = Blog.query.all()
 
     if request.method == 'POST':
-            
-        post = request.form['post']
-        posts.append(post)
-        
-        
-        return render_template('blog.html', title="fromnewpost", posts=posts)
 
-    return render_template("newpost.html", title="WTF", posterror=posterror)
+        #look inside the html form for user data    
+        title = request.form['title']
+        post = request.form['post']
+        
+        #creat a newpost reference 
+        newpost = Blog(title, post)
+                       
+
+        db.session.add(newpost)
+        db.session.commit()
+
+        #creat a link to go to the new post when use clicks submit
+        postlink = "/blog?id=" + str(newpost.id)
+
+        return redirect(postlink)
+
+    return render_template('newpost.html', title="WTF", posterror=posterror)
                 
 
 
